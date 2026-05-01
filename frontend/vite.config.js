8d4b42601c7a7f6633a9adbd2ba6506419497e36
Tavan Mohammed <tavan.mohammed19@gmail.com>
2026-04-30 21:01:14 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+
 
 export default defineConfig({
   plugins: [react()],
